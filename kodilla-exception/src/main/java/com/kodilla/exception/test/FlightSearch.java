23afa3518cd90be83621ac5f202ba4cb4f1f3b69
Shaq1256 <lukaszmioduszewskii@gmail.com>
2019-11-12 14:37:39 +0000

    zad 8.4

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightSearch.java
@@ -0,0 +1,27 @@
+package com.kodilla.exception.test;
+
+import javax.management.relation.RoleInfoNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class FlightSearch {
+
+    public Map<String, Boolean> initDirections() {
+        Map<String, Boolean> directions = new HashMap<>();
+        directions.put("Warsaw", true);
+        directions.put("London", true);
+        directions.put("New York", false);
+        directions.put("Rome", true);
+
+        return directions;
+    }
+    Boolean findFlight(Flight flight) throws RoleInfoNotFoundException {
+        Map<String, Boolean> directions = initDirections();
+        if(!directions.entrySet().contains(flight.getDepartureAirport()) || !directions.entrySet().contains(flight.getArrivalAirport())) {
+            throw new RoleInfoNotFoundException();
+        } else if (directions.containsValue(false)) {
+            throw new RoleInfoNotFoundException();
+        }
+        return true;
+    }
+}
--- kodilla-exception/src/main/java/com/kodilla/exception/test/SecondChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/SecondChallenge.java
@@ -1,7 +1,5 @@
 package com.kodilla.exception.test;
 
-import static java.lang.System.*;
-
 public class SecondChallenge {
     public String probablyIWillThrowException(double x, double y) throws Exception {
         if(x >= 2 || x < 1 || y == 1.5) {
