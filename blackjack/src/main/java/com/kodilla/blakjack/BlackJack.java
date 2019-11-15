ce2a789b044457cd067612b2ff349c3e26e7a674
Shaq1256 <lukaszmioduszewskii@gmail.com>
2019-11-15 21:27:34 +0000

    BlackJack

--- blackjack/src/main/java/com/kodilla/blakjack/BlackJack.java
+++ blackjack/src/main/java/com/kodilla/blakjack/BlackJack.java
@@ -1,32 +1,66 @@
 package com.kodilla.blakjack;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class BlackJack extends Application{
 
-    private Image imageback = new Image("file:C:\\Users\\Lukasz\\Downloads\\images.jpg");
+    private Image imageback = new Image("file:C:\\Users\\Lukasz\\IdeaProjects\\MyProjects\\kodilla-course2\\resources\\images.jpg");
+    private Image card = new Image("file:C:\\Users\\Lukasz\\IdeaProjects\\MyProjects\\kodilla-course2\\resources\\karta.jpg");
+    private FlowPane cards = new FlowPane(Orientation.HORIZONTAL);
 
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) {
-        Group root = new Group();
-        Scene scene = new Scene(root, 300, 300, Color.BLACK);
+    public void start(Stage primaryStage) throws Exception{
 
-        Rectangle r = new Rectangle(25, 25, 250, 250);
-        r.setFill(Color.BLUE);
+        BackgroundSize backgroundSize = new BackgroundSize(100, 100, true, true,
+                true, false);
+        BackgroundImage backgroundImage = new BackgroundImage(imageback, BackgroundRepeat.REPEAT,
+                BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
+        Background background = new Background(backgroundImage);
 
-        root.getChildren().add(r);
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setPadding(new Insets(11.5, 12.5, 13.5, 14.5));
+        grid.setHgap(5.5);
+        grid.setVgap(5.5);
+        grid.setBackground(background);
+
+        ImageView img = new ImageView(card);
+        cards.getChildren().add(img);
+
+        grid.add(cards, 0, 0, 3, 1);
+
+        Scene scene = new Scene(grid, 1600, 900, Color.BLACK);
+
         primaryStage.setTitle("BlackJack");
         primaryStage.setScene(scene);
         primaryStage.show();
+
+
+
+//        Group root = new Group();
+//        Scene scene = new Scene(root, 300, 300, Color.BLACK);
+//
+//        Rectangle r = new Rectangle(25, 25, 250, 250);
+//        r.setFill(Color.BLUE);
+
+//        root.getChildren().add(r);
+//        primaryStage.setTitle("BlackJack");
+//        primaryStage.setScene(scene);
+//        primaryStage.show();
     }
 }
