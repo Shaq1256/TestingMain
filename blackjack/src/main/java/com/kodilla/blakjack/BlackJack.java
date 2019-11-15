a067f9c84ff24cf573988204710c4347cd12b769
Shaq1256 <lukaszmioduszewskii@gmail.com>
2019-11-15 19:41:52 +0000

    BlackJack

---
+++ blackjack/src/main/java/com/kodilla/blakjack/BlackJack.java
@@ -0,0 +1,32 @@
+package com.kodilla.blakjack;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+public class BlackJack extends Application{
+
+    private Image imageback = new Image("file:C:\\Users\\Lukasz\\Downloads\\images.jpg");
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Group root = new Group();
+        Scene scene = new Scene(root, 300, 300, Color.BLACK);
+
+        Rectangle r = new Rectangle(25, 25, 250, 250);
+        r.setFill(Color.BLUE);
+
+        root.getChildren().add(r);
+        primaryStage.setTitle("BlackJack");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
--- kodilla-exception/src/main/java/com/kodilla/exception/test/FlightSearch.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightSearch.java
@@ -1,6 +1,5 @@
 package com.kodilla.exception.test;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
