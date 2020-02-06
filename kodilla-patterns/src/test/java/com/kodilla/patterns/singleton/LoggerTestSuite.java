88ad77b8adbfe00e1d3d6c1d45697471ae277084
Shaq1256 <lukaszmioduszewskii@gmail.com>
2020-02-06 10:53:24 +0000

    Zad 15.1

--- kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -1,5 +1,25 @@
 package com.kodilla.patterns.singleton;
 
-public class LoggerTestSuite {
+import org.junit.*;
 
+public class LoggerTestSuite {
+    @After
+    public void after() {
+        System.out.println("Test case end.");
+    }
+
+    @Before
+    public void before() {
+        System.out.println("Test case begin");
+    }
+
+    @Test
+    public void testGetLastLog() {
+        //Given
+        Logger.getInstance().log("Ostatni wpis.");
+        //When
+        String lastLog = Logger.getInstance().getLastLog();
+        //Then
+        Assert.assertEquals("Ostatni wpis.", lastLog);
+    }
 }
