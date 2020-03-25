9bae081ab9a0a408aed98baa08d7517f166f29a0
Shaq1256 <lukaszmioduszewskii@gmail.com>
2020-03-25 19:03:46 +0000

    Zad 17.3 working

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/invoice/dao/InvoiceDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/invoice/dao/InvoiceDaoTestSuite.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import javax.transaction.Transactional;
 import java.math.BigDecimal;
 
 @RunWith(SpringRunner.class)
@@ -17,6 +16,7 @@ import java.math.BigDecimal;
 public class InvoiceDaoTestSuite {
     @Autowired
     InvoiceDao invoiceDao;
+    @Autowired
     ItemDao itemDao;
 
     @Test
@@ -44,6 +44,7 @@ public class InvoiceDaoTestSuite {
         Assert.assertEquals(2, invoiceSize + invoiceSize1);
 
         //CleanUp
+        invoiceDao.deleteAll();
         itemDao.deleteAll();
     }
 }
